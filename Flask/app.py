f8267de30128ea9bac67525b82a48e944821965e
Aurélie Gallet <aureliebeijiagallet@gmail.com>
2024-11-20 18:37:50 +0100

    Made dummy pages and added a nicer hello world display

--- Flask/app.py
+++ Flask/app.py
@@ -3,5 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('hello.html')
+def show_home():
+    return render_template('home.html')
+
+@app.route("/system")
+def show_system():
+    return render_template('system.html')
+
+@app.route("/expert")
+def show_expert():
+    return render_template('expert.html')
+
+@app.route("/knowledge")
+def show_knowlegde():
+    return render_template('knowledge.html')
