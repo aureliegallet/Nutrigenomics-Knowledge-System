342462be1904070f64524e3ba99ae12eddff0006
Aurélie Gallet <aureliebeijiagallet@gmail.com>
2025-01-14 11:09:08 +0100

    Small message on results page

--- Flask/static/js/quiz.js
+++ Flask/static/js/quiz.js
@@ -129,6 +129,7 @@ function nextQuestion(next) {
                     <p>Your gut health is ${health}.</p><br/>
                     <h2>Suggestions for Improvement:</h2>
                     <ul id="suggestions"></ul>
+                    <p> Make sure to look at our Knowledge Page to find out specifically how to improve your diet. </p>
                     </br>
                     <img src="../static/img/gut.png" alt="gut" width=90%>
                     <p>Image taken from Microsoft Designer<p><br/>
